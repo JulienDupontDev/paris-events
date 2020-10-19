8f595787df4dc5b132b84c5fc83e51da5d78a233
Julien Dupont <julien.dupont2018@gmail.com>
2020-10-20 01:01:23 +0200

    feat(Navbar): added navbar and updated file structure)

---
+++ src/components/navbar/index.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade, withStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyle = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow:1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        flexGrow:2,
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '20ch',
+            '&:focus': {
+                width: '40ch',
+            },
+        },
+    },
+});
+
+class SearchAppBar extends Component {
+
+    inputHandler = event => {
+        console.log(event.target.value);
+    }
+    render() {
+
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.root} >
+                <AppBar position="fixed">
+                    <Toolbar>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            Paris évènements
+                </Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase
+                                placeholder="Rechercher"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                                inputProps={{ 'aria-label': 'rechercher' }}
+                                onKeyDown={this.inputHandler}
+                            />
+                        </div>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+
+}
+
+export default withStyles(useStyle)(SearchAppBar);
