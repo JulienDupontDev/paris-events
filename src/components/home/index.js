54539f4b2eee7e603f6051ace62fd7d529bca3c6
Julien Dupont <julien.dupont2018@gmail.com>
2020-11-05 22:13:50 +0100

    feat(TableFilter): added table for filtering options with state management

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,6 +2,11 @@ import Grid from '@material-ui/core/Grid';
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
+import parisAPI from '../../api/paris';
+import Sorting from '../tableSearch';
+import EventCard from '../eventCard';
+import Iframe from 'react-iframe';
 
 const useStyle = theme => ({
   root: {
@@ -19,22 +24,38 @@ const useStyle = theme => ({
 class Home extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      events: [],
+    }
+  }
+
+  async componentDidMount() {
+    const paris = new parisAPI();
+    await paris.getEvents().then(res => this.setState({ ...this.state, events: "cucou" }))
+    console.log(this.state)
+
   }
 
 
   render() {
-    const { classes } = this.props;
-    const {
-      searchTerm,
-      events,
-    } = this.props;
+    const { classes, searchTerm } = this.props;
+
     return (
       <Grid className={classes.root}>
-        <Grid item  xs={12} sm={10}>
+        <Grid item xs={12} sm={12}>
           <Typography>Bienvenue ! Recherchez des évènements à Paris ci-dessous.</Typography>
         </Grid>
-        <Grid item>
+        {/* <Iframe url="https://opendata.paris.fr/explore/embed/dataset/que-faire-a-paris-/map/?disjunctive.category&disjunctive.tags&disjunctive.address_zipcode&disjunctive.address_city&disjunctive.access_type&disjunctive.price_type&disjunctive.address_name&rows=1&dataChart=eyJxdWVyaWVzIjpbeyJjaGFydHMiOlt7InR5cGUiOiJ0cmVlbWFwIiwiZnVuYyI6IkNPVU5UIiwieUF4aXMiOiJwbXIiLCJzY2llbnRpZmljRGlzcGxheSI6dHJ1ZSwiY29sb3IiOiJyYW5nZS1jdXN0b20iLCJwb3NpdGlvbiI6ImNlbnRlciJ9XSwieEF4aXMiOiJjYXRlZ29yeSIsIm1heHBvaW50cyI6NTAsInRpbWVzY2FsZSI6IiIsInNvcnQiOiIiLCJzZXJpZXNCcmVha2Rvd24iOiIiLCJzZXJpZXNCcmVha2Rvd25UaW1lc2NhbGUiOiIiLCJjb25maWciOnsiZGF0YXNldCI6InF1ZS1mYWlyZS1hLXBhcmlzLSIsIm9wdGlvbnMiOnsiZGlzanVuY3RpdmUuY2F0ZWdvcnkiOnRydWUsImRpc2p1bmN0aXZlLnRhZ3MiOnRydWUsImRpc2p1bmN0aXZlLmFkZHJlc3NfemlwY29kZSI6dHJ1ZSwiZGlzanVuY3RpdmUuYWRkcmVzc19jaXR5Ijp0cnVlLCJkaXNqdW5jdGl2ZS5hY2Nlc3NfdHlwZSI6dHJ1ZSwiZGlzanVuY3RpdmUucHJpY2VfdHlwZSI6dHJ1ZSwiZGlzanVuY3RpdmUuYWRkcmVzc19uYW1lIjp0cnVlLCJyb3dzIjoiMSJ9fX1dLCJkaXNwbGF5TGVnZW5kIjp0cnVlLCJhbGlnbk1vbnRoIjp0cnVlLCJ0aW1lc2NhbGUiOiIifQ%3D%3D&basemap=jawg.dark&location=14,48.86342,2.35773&static=false&datasetcard=false&scrollWheelZoom=true"
+          height="450px"
+          width="450px"
+          display='initial'
+          position="relative" /> */}
+        <Sorting />
+
+        <Grid item sm={5}>
           {searchTerm}
+          {/* {this.state.events}
+          <EventCard /> */}
         </Grid>
       </Grid>
     );
---
+++ src/components/tableSearch/index.js
@@ -0,0 +1,448 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import Grid from '@material-ui/core/Grid';
+import { FormControl, FormControlLabel, FormGroup, Switch } from '@material-ui/core';
+import axios from 'axios';
+
+
+const useStyles = (theme) => ({
+  root: {
+    padding: "40px",
+    display: "flex",
+    justifyContent: "center"
+  },
+  // formControl: {
+  //   margin: theme.spacing(1),
+  //   minWidth: 120,
+  // },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+});
+
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
+
+class Sorting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pmr: 0,
+      blind: 0,
+      deaf: 0,
+      categories: availableChoices.categories,
+      postCodes: [],
+      cities: [],
+      places: [],
+      subCategories: [],
+      userFilters: {},
+      tags: availableChoices.tags
+    }
+  }
+
+  componentDidMount() {
+
+    axios.get('https://opendata.paris.fr/api/records/1.0/search/?disjunctive.category=true&disjunctive.tags=true&disjunctive.address_zipcode=true&disjunctive.address_city=true&disjunctive.access_type=true&disjunctive.price_type=true&disjunctive.address_name=true&refine.category=Animations+&refine.category=Concerts+&refine.category=%C3%89v%C3%A9nements+&refine.category=Expositions+&refine.category=Spectacles+&rows=0&facet=category&facet=tags&facet=address_name&facet=address_zipcode&facet=address_city&facet=pmr&facet=blind&facet=deaf&facet=access_type&facet=price_type&facetsort.category=alphanum&facetsort.tags=alphanum&facetsort.address_name=alphanum&facetsort.address_zipcode=alphanum&facetsort.address_city=alphanum&facetsort.access_type=alphanum&facetsort.price_type=alphanum&dataset=que-faire-a-paris-&timezone=Europe%2FBerlin&lang=fr').then((response) => {
+      console.log(response.data.facet_groups)
+      const facets = response.data.facet_groups;
+      this.setState(
+        {
+          categories: facets.filter((facet) => facet.name === 'category')[0].facets,
+          types: facets.filter((facet) => facet.name.includes('type')),
+          postCodes: facets.filter((facet) => facet.name === 'address_zipcode')[0].facets,
+          cities: facets.filter((facet) => facet.name === 'address_city')[0].facets,
+          places: facets.filter((facet) => facet.name === 'address_name')[0].facets
+        }
+      )
+
+      console.log(Object.values(this.state.userFilters).length)
+      let userfilters = { ...this.state.userFilters }
+      userfilters['test'] = "1"
+      userfilters.mom = "1"
+
+      this.setState({ userFilters: userfilters });
+      console.log(this.state.userFilters)
+
+    });
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Grid container spacing={2} className={classes.root}>
+        <Grid item xs={6} sm={4} >
+          <Autocomplete
+            multiple
+            noOptionsText="Pas d'options"
+            id="categories"
+            key="1"
+            limitTags={2}
+            options={this.state.categories}
+            disableCloseOnSelect
+            onChange={(event, values) => {
+              if (values.length === 0) {
+                this.setState({ ...this.state, subCategories: [] })
+                return;
+              }
+              let tempArray = [];
+              values.forEach((category) => {
+                let subTempArray =
+                  // tempArray = tempArray.concat(
+                  category.facets.filter(
+                    (subCategory) => !this.state.subCategories.includes(subCategory.name));
+                subTempArray.forEach((subCategory) => subCategory.category = category.name)
+                tempArray = tempArray.concat(subTempArray)
+              }
+              )
+
+
+              this.setState({ ...this.state, subCategories: tempArray })
+              // console.log(this.state.subCategories)
+
+              // this.setState({...this.state, subCategories: tempArray })
+              // console.log(this.state.subCategories)
+            }
+            }
+            getOptionLabel={(option) => option.name}
+            renderOption={(option, { selected }) => (
+              <React.Fragment>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.name}
+              </React.Fragment>
+            )}
+            renderInput={(params) => (
+              <TextField {...params} variant="outlined" label="Catégories" placeholder="Catégorie" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Autocomplete
+            multiple
+            noOptionsText="Pas d'options"
+            id="subCategories"
+            key="2"
+            limitTags={2}
+            options={this.state.subCategories}
+            groupBy={(subCategory) => subCategory.category}
+            disableCloseOnSelect
+            onChange={(event, values) => console.log(values)}
+            getOptionLabel={(subCategory) => subCategory.name}
+            renderOption={(option, { selected }) => {
+              console.log(this.state.subCategories)
+              return (
+                <React.Fragment>
+                  <Checkbox
+                    icon={icon}
+                    checkedIcon={checkedIcon}
+                    style={{ marginRight: 8 }}
+                    checked={selected}
+                  />
+                  {option.name}
+                </React.Fragment>
+              )
+            }
+            }
+            renderInput={(params) => (
+              <TextField {...params} variant="outlined" label="Sous-catégories" placeholder="Sous-catégorie" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Autocomplete
+            multiple
+            noOptionsText="Pas d'options"
+            id="tags"
+            limitTags={2}
+            options={this.state.tags}
+            disableCloseOnSelect
+            getOptionLabel={(tag) => tag}
+            renderOption={(option, { selected }) => (
+              <React.Fragment>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option}
+              </React.Fragment>
+            )
+            }
+            renderInput={(params) => (
+              <TextField {...params} variant="outlined" label="Mots-clés" placeholder="Mots-clés" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Autocomplete
+            multiple
+            noOptionsText="Pas d'options"
+            id="placeName"
+            limitTags={2}
+            options={this.state.places}
+            disableCloseOnSelect
+            getOptionLabel={(place) => place.name}
+            renderOption={(option, { selected }) => (
+              <React.Fragment>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.name}
+              </React.Fragment>
+            )
+            }
+            renderInput={(params) => (
+              <TextField {...params} variant="outlined" label="Nom du lieu" placeholder="Nom du lieu" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4} >
+          <Autocomplete
+            multiple
+            noOptionsText="Pas d'options"
+            id="postCode"
+            limitTags={2}
+            options={this.state.postCodes}
+            disableCloseOnSelect
+            onChange={(event, values) => {
+              // this.setState({ ...this.state, subCategories: tempArray })
+            }
+            }
+            getOptionLabel={(option) => option.name}
+            renderOption={(option, { selected }) => (
+              <React.Fragment>
+                <Checkbox
+                  icon={icon}
+                  checkedIcon={checkedIcon}
+                  style={{ marginRight: 8 }}
+                  checked={selected}
+                />
+                {option.name}
+              </React.Fragment>
+            )}
+            renderInput={(params) => (
+              <TextField {...params} variant="outlined" label="Code postal" placeholder="Code postal" />
+            )}
+          />
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Autocomplete
+            multiple
+            noOptionsText="Pas d'options"
+            id="city"
+            limitTags={2}
+            options={this.state.cities}
+            disableCloseOnSelect
+            onChange={(event, values) => console.log(values)}
+            getOptionLabel={(city) => city.name}
+            renderOption={(option, { selected }) => {
+              return (
+                <React.Fragment>
+                  <Checkbox
+                    icon={icon}
+                    checkedIcon={checkedIcon}
+                    style={{ marginRight: 8 }}
+                    checked={selected}
+                  />
+                  {option.name}
+                </React.Fragment>
+              )
+            }
+            }
+            renderInput={(params) => (
+              <TextField {...params} variant="outlined" label="Ville" placeholder="Ville" />
+            )}
+          />
+        </Grid>
+        <Grid item>
+          <FormControl component="fieldset">
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={this.state.pmr} onChange={() => this.setState({ ...this.state, pmr: this.state.pmr === 0 ? 1 : 0 })} name="pmr" />}
+                label="Mobilité réduite"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl component="fieldset">
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={this.state.blind} onChange={() => this.setState({ ...this.state, blind: this.state.blind === 0 ? 1 : 0 })} name="blind" />}
+                label="Accès mal voyant"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <FormControl component="fieldset">
+            <FormGroup>
+              <FormControlLabel
+                control={<Switch checked={this.state.deaf} onChange={() => this.setState({ ...this.state, deaf: this.state.deaf === 0 ? 1 : 0 })} name="deaf" />}
+                label="Accès mal entendant"
+              />
+            </FormGroup>
+          </FormControl>
+        </Grid>
+
+        {this.state.types ? this.state.types.forEach((type) => (
+          <Grid item sm={2}>
+            <Autocomplete
+              multiple
+              noOptionsText="Pas d'options"
+              id={type.name}
+              limitTags={2}
+              options={this.state.types}
+              disableCloseOnSelect
+              getOptionLabel={(type) => type.name}
+              renderOption={(option, { selected }) => (
+                <React.Fragment>
+                  <Checkbox
+                    icon={icon}
+                    checkedIcon={checkedIcon}
+                    style={{ marginRight: 8 }}
+                    checked={selected}
+                  />
+                  {option.name}
+                </React.Fragment>
+              )
+              }
+              renderInput={(params) => (
+                <TextField {...params} variant="outlined" label={type.name} placeholder="Mots-clés" />
+              )}
+            />
+          </Grid>
+        )) : null}
+      </Grid>
+    );
+  }
+
+}
+
+const availableChoices = {
+  categories: [
+    {
+      label: 'Animations',
+      subCategories: [
+        {
+          label: 'Atelier / Cours',
+        }
+      ]
+    },
+    {
+      label: 'Concerts',
+      subCategories: [
+        {
+          label: 'Chanson française',
+
+        }
+      ]
+    },
+    {
+      label: 'Évènements',
+      subCategories: [
+        {
+          label: 'Chanson française',
+
+        }
+      ]
+    },
+    {
+      label: 'Expositions',
+      subCategories: [
+        {
+          label: 'Chanson française',
+
+        }
+      ]
+    },
+    {
+      label: 'Spectacles',
+      subCategories: [
+        {
+          label: 'Chanson française',
+        }
+      ]
+    },
+  ],
+  tags: [
+    'Ados',
+    'Bibliothèques',
+    'Cinéma',
+    'En Famille',
+    'Enfants',
+    'English',
+    'Étudiants',
+    'Expos',
+    'Geek',
+    'Gourmand',
+    'Insolite',
+    'Les Nuits',
+    'Musique',
+    'Noël',
+    'Plein air',
+    'Queer Lgbt',
+    'Solidaire',
+    'Sport',
+    'Urbain',
+    'Végétalisons Paris',
+  ],
+  accessibility: [
+    {
+      label: 'Accès PMR',
+      queryValue: 'pmr',
+      value: false,
+    },
+    {
+      label: 'Accès mal voyant',
+      queryValue: 'blind',
+      value: false,
+    },
+    {
+      label: 'Accès mal entendant',
+      queryValue: 'deaf',
+      value: false,
+    }
+  ],
+  types: [
+    {
+      label: 'Type d\'acc\u00E8s',
+      queryValue: 'access_type',
+      availableValues: [
+        'conseillé',
+        'libre',
+        'réservation'
+      ],
+      selectedValues: []
+    },
+    {
+      label: 'Type de prix',
+      queryValue: 'price_type',
+      availableValues: [
+        'gratuit',
+        'payant',
+      ],
+      selectedValues: []
+    },
+
+  ]
+
+}
+
+
+export default withStyles(useStyles)(Sorting);
